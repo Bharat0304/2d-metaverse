5b2b3c22b565b0e13055bfb61e53d5fb6807597e
Bharat Kumar <k.bharatkumar815@gmail.com>
2026-05-05 19:25:19 +0530

    fixed auth

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { authRouter } from './routes/v1/auth.js';
 import { spaceRouter } from './routes/v1/space.js';
 const app = express();
 import dotenv from 'dotenv/config';
 const port = 4000;
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth",authRouter);
 app.use("/api/v1/space", spaceRouter);
